f783808f63ff759c6780bde04102ee31f1d213d2
Dheeraj <dheerajjoshi9339@gmail.com>
2023-03-31 15:13:34 +0530

    intial

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function topDetails(){
+    document.getElementById("contact-list").style.display = "block";
+}
+
+function hideTopDetails(){
+    document.getElementById("contact-list").style.display = "none";
+}
+
+function showContent(){
+    document.getElementById("nav-content").style.display = "block";
+}
+
+function hideShowContent(){
+    document.getElementById("nav-content").style.display = "none";
+}
